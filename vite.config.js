161bc8cb5bf16934b100a9fb2016bc643a885898
Orange <72426886+Awu1227@users.noreply.github.com>
2024-08-27 21:27:15 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 // vite.config.js
 import { defineConfig } from "vite";
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import vue from '@vitejs/plugin-vue'
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import vue from "@vitejs/plugin-vue";
 export default defineConfig({
+  base: "/wms_3d/",
   plugins: [
     vue(),
     AutoImport({
@@ -12,7 +13,7 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()],
-    })
+    }),
   ],
   build: {
     cssCodeSplit: true, // 是否提取所有CSS到一个CSS文件中, introduct: https://cn.vitejs.dev/config/build-options.html#build-csscodesplit
